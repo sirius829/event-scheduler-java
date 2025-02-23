18a928e878f26285b5502fc6060f34e3e7b1360e
sirius829 <jacob.townsend.work@gmail.com>
2025-02-23 22:23:36 +0100

    init

---
+++ src/main/java/com/jacob/event_scheduler/EventSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.jacob.event_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacob/event_scheduler/EventSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.event_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
